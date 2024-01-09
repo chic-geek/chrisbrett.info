c7aff02496c4af70740677e5c822df1ec1b7b57e
Chris Brett <c.brett84@gmail.com>
2024-01-09 21:46:58 +0000

    [refactor]: a11y friendly amber highlight

--- app/utils/get-random-color.ts
+++ app/utils/get-random-color.ts
@@ -1,5 +1,5 @@
 const PRESET_HIGHLIGHT_COLORS = [
-  "251, 191, 36", // amber   [400]
+  "245, 158, 11", // amber   [500]
   "16, 185, 129", // emerald [500]
   "59, 130, 246", // blue    [500]
   "232, 121, 249", // fuscia  [400]
