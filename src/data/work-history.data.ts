298947e51df1b56f276a5f9264c60fb642438597
Chris Brett <c.brett84@gmail.com>
2024-02-19 10:21:57 +0000

    fix(data): typo in ca work history data

--- src/data/work-history.data.ts
+++ src/data/work-history.data.ts
@@ -1,74 +1,74 @@
 export const workHistoryData = [
   {
-    "id": "1",
-    "company": {
-      "name": "Qatalog",
-      "url": "https://qatalog.com/"
+    id: "1",
+    company: {
+      name: "Qatalog",
+      url: "https://qatalog.com/",
     },
-    "position": "Frontend Developer",
-    "startDate": "2022-02-01",
-    "endDate": "2023-08-30",
-    "lede": "Qatalog is an AI-driven enterprise solution aimed at enhancing business efficiency and decision-making.",
-    "contributions": [
+    position: "Frontend Developer",
+    startDate: "2022-02-01",
+    endDate: "2023-08-30",
+    lede: "Qatalog is an AI-driven enterprise solution aimed at enhancing business efficiency and decision-making.",
+    contributions: [
       "Led the development process of reworking the application for mobile devices, ensuring that essential features were accessible across a range of devices where previously they were not. This involved working closely with the design team to ensure the user experience was consistent and intuitive.",
       "Contributed to developing and expanding the internal React component library, enhancing platform scalability and maintainability.",
       "Collaborated in the re-architecting of the client-side application, focusing on enhancing code reusability and integrating an upgraded component/design system.",
       "Successfully implemented a customised @mention feature, building on top of a heavily modified TipTap installation to enhance the user experience and engagement.",
-      "Demonstrated adaptability and a thirst for learning by embracing new technologies, Typescript, styled-components, Apollo client for handling data fetching, caching and optimistic UI updates and also an introduction to Next.js."
-    ]
+      "Demonstrated adaptability and a thirst for learning by embracing new technologies, Typescript, styled-components, Apollo client for handling data fetching, caching and optimistic UI updates and also an introduction to Next.js.",
+    ],
   },
   {
-    "id": "2",
-    "company": {
-      "name": "Monolith.xyz",
-      "url": "https://monolith.xyz/"
+    id: "2",
+    company: {
+      name: "Monolith.xyz",
+      url: "https://monolith.xyz/",
     },
-    "position": "Frontend Developer",
-    "startDate": "2021-11-01",
-    "endDate": "2022-01-31",
-    "lede": "European decentralised finance account, allowing you to buy, spend and hold cyrpto-currency.",
-    "contributions": [
+    position: "Frontend Developer",
+    startDate: "2021-11-01",
+    endDate: "2022-01-31",
+    lede: "European decentralised finance account, allowing you to buy, spend and hold cyrpto-currency.",
+    contributions: [
       "In the process of developing new features, translating designs into code, and addressing issues, this position exposed me to Typescript and the React Native framework.",
-      "Build new application features as part of the Token.com crypto asset application."
-    ]
+      "Build new application features as part of the Token.com crypto asset application.",
+    ],
   },
   {
-    "id": "3",
-    "company": {
-      "name": "Citizens Advice",
-      "url": "https://www.citizensadvice.org.uk/"
+    id: "3",
+    company: {
+      name: "Citizens Advice",
+      url: "https://www.citizensadvice.org.uk/",
     },
-    "position": "Frontend Developer",
-    "startDate": "2017-04-01",
-    "endDate": "2021-10-31",
-    "lede": "Citizens Advice is a network of independent charities throughout the UK offering free, confidential information and advice.",
-    "contributions": [
+    position: "Frontend Developer",
+    startDate: "2017-04-01",
+    endDate: "2021-10-31",
+    lede: "Citizens Advice is a network of independent charities throughout the UK offering free, confidential information and advice.",
+    contributions: [
       "Design and development of the Citizens Advice design system. Creating a collection of functional CSS classes and components for large scale code reuse and consistency across digital products.",
       "Leading new feature development and contributing to existing features of a bespoke case management system using Ruby, Rails and React.",
       "Mentor junior team members, giving guidance on solving technical problems and planning features, ultimately giving them freedom to take more of a leadership role in other projects.",
       "Discovery and development for a new CMS to scale content and it's distribution.",
       "Taking a personal React Component library project and turning it into a reusable library for other members of the team.",
       "The maintenance of public facing and adviser only websites.",
-      "A suite of interactive advice tools built using ReactJS."
-    ]
+      "A suite of interactive advice tools built using ReactJS.",
+    ],
   },
   {
-    "id": "4",
-    "company": {
-      "name": "NewsNow",
-      "url": "https://www.newsnow.co.uk/"
+    id: "4",
+    company: {
+      name: "NewsNow",
+      url: "https://www.newsnow.co.uk/",
     },
-    "position": "Web Developer",
-    "startDate": "2016-03-01",
-    "endDate": "2017-03-28",
-    "lede": "The UK's premier homegrown, independent, fully automated news aggregator.",
-    "contributions": [
+    position: "Web Developer",
+    startDate: "2016-03-01",
+    endDate: "2017-03-28",
+    lede: "The UK's premier homegrown, independent, fully automated news aggregator.",
+    contributions: [
       "Building out UI features like a revealing navigation, new cookie control features and the headline alert system, all of which can be seen on the homepage.",
       "Building new features using their custom Perl templating language, CL and bug fixes within the legacy code.",
       "Maintain the legacy CSS codebase. No pre / post processing. I adopted a BEM style to try to stop the leaking of styling across unintended parts of the UI / CSS codebase.",
       "Designing and prototyping mobile first templates for future overview pages.",
       "A brand refresh including logo, colours and usage guidelines.",
-      "Systems administration on custom Debian servers and on site server maintenance for repairs or component replacements."
-    ]
-  }
-]
+      "Systems administration on custom Debian servers and on site server maintenance for repairs or component replacements.",
+    ],
+  },
+];
