0c866987a3d56fa75c1fee5ac326971899047950
Chris Brett <c.brett84@gmail.com>
2024-02-28 10:55:27 +0000

    refactor(footer): styling amends to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -11,10 +11,12 @@ export function Footer() {
     <footer className="mt-12 border-t border-[rgba(75,85,99,0.3)]">
       <Container>
         <div className="flex items-center justify-between py-5 md:py-10">
-          <div className="flex items-center gap-x-2 text-[rgb(75,85,99)] max-[400px]:text-sm">
+          <div className="flex items-center gap-x-1 text-[rgb(75,85,99)] max-[400px]:text-sm">
             <CopyrightIcon size="16" />
-            <div>chrisbrett.info</div>
             <div>{currentYear}</div>
+            <a href="/" className="underline">
+              chrisbrett.info
+            </a>
             <div className="hidden">v{appVersion}</div>
           </div>
           <SocialIcons />
