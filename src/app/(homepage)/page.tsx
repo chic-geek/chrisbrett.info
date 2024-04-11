12f2f31b85c84ae0b380a8d7d4e2ce8aa96eb441
Chris Brett <c.brett84@gmail.com>
2024-04-11 09:23:51 +0000

    refactor(blog): update url for posts

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,6 +1,7 @@
 import { HeroSection } from "./_components/hero-section";
 import { HistoryItemsSection } from "./_components/history-items-section";
 import { CodeExamplesSection } from "./_components/code-examples-section";
+import { GoogleSigninButton } from "@/components/google-signin-button";
 
 export default function Homepage() {
   return (
