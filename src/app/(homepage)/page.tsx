ae7757e212cd686ce12cdf422ad1273f4777224e
Chris Brett <c.brett84@gmail.com>
2024-04-11 09:28:52 +0000

    fix(homepage): bug caused by missing import

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,7 +1,6 @@
 import { HeroSection } from "./_components/hero-section";
 import { HistoryItemsSection } from "./_components/history-items-section";
 import { CodeExamplesSection } from "./_components/code-examples-section";
-import { GoogleSigninButton } from "@/components/google-signin-button";
 
 export default function Homepage() {
   return (
