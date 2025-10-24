3929c4d5f1987c9dd03ecdde1ea7cc0108e42b68
Chris Brett <chic-geek@users.noreply.github.com>
2025-10-24 22:22:28 +0100

    refactor(blog): make code blocks full width (#17)
    * refactor(blog): make code blocks full width
    
    * fix(blog): mistyped classnames

--- src/app/(blog)/articles/[slug]/_components/prose.tsx
+++ src/app/(blog)/articles/[slug]/_components/prose.tsx
@@ -19,8 +19,12 @@ const BASE_PROSE_STYLES = [
   "prose-p:text-base",
   "prose-p:font-normal",
   "prose-p:leading-[1.75]",
-  "prose-p:text-gray-100",
+  "prose-p:text-gray-300",
+  "prose-p:max-w-[65ch]",
+  "prose-p:text-pretty",
   "prose-hr:border-gray-600/30",
+  "prose-headings:max-w-[65ch]",
+  "prose-headings:text-pretty",
 ];
 
 interface ProseProps {
@@ -28,5 +32,7 @@ interface ProseProps {
 }
 
 export function Prose({ children }: ProseProps) {
-  return <div className={cn("py-14", BASE_PROSE_STYLES)}>{children}</div>;
+  return (
+    <div className={cn("max-w-none py-14", BASE_PROSE_STYLES)}>{children}</div>
+  );
 }
